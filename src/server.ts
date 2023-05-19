18be2acc984e68bba4f3c14a624ec62d4a858e17
kielkow <matheuskiel@fiorifer.com.br>
2023-05-19 13:28:00 -0300

    chore: config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/status', () => {
-    return 'server is running';
-});
+  return 'server is running'
+})
 
-app.listen(
-    { port: 3333 }
-).then(
-    () => console.log('[server is running]')
-);
+app.listen({ port: 3333 }).then(() => console.log('[server is running]'))
