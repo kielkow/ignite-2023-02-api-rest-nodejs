014bce571de73d68d5794d2a6a37130712d59407
kielkow <matheuskiel@fiorifer.com.br>
2023-05-19 10:56:14 -0300

    chore: install chore dependencies

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/status', () => {
+    return 'server is running';
+});
+
+app.listen(
+    { port: 3333 }
+).then(
+    () => console.log('[server is running]')
+);
