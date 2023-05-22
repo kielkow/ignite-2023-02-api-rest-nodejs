561626d0259d9f24a8949370d200f3ae862e0af1
kielkow <matheuskiel@fiorifer.com.br>
2023-05-22 17:22:03 -0300

    feat: config knex setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/status', () => {
   return 'server is running'
+})
+
+app.get('/status-database', () => {
+  return knex('sqlite_schema').select('*')
 })
 
 app.listen({ port: 3333 }).then(() => console.log('[server is running]'))
