62360685c243b758629922d520b8cb5df4e30108
kielkow <matheuskiel@fiorifer.com.br>
2023-05-24 12:48:29 -0300

    feat: routes POST/GET transaction

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
 import fastify from 'fastify'
+import { randomUUID } from 'crypto'
+
 import { knex } from './database'
+
+interface ITransactionBody {
+  title: string
+  amount: number
+}
+
+interface ITransactionQueryString {
+  title: string
+  amount: number
+}
 
 const app = fastify()
 
@@ -9,6 +21,44 @@ app.get('/status', () => {
 
 app.get('/status-database', () => {
   return knex('sqlite_schema').select('*')
+})
+
+app.get<{ Querystring: ITransactionQueryString }>(
+  '/transactions',
+  (req, res) => {
+    const { title, amount } = req.query
+
+    if (title && !amount) {
+      return knex('transactions').select('*').where('title', title)
+    }
+
+    if (amount && !title) {
+      return knex('transactions').select('*').where('amount', amount)
+    }
+
+    if (title && amount) {
+      return knex('transactions').select('*').where({
+        title,
+        amount,
+      })
+    }
+
+    return knex('transactions').select('*')
+  },
+)
+
+app.post<{ Body: ITransactionBody }>('/transactions', {}, async (req, res) => {
+  const { title, amount } = req.body
+
+  const transaction = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title,
+      amount,
+    })
+    .returning('*')
+
+  res.send(transaction)
 })
 
 app.listen({ port: 3333 }).then(() => console.log('[server is running]'))
