9b032ad9d3f76e6465d018bcae0e9e70dc3ad74f
kielkow <matheuskiel@fiorifer.com.br>
2023-05-25 09:32:41 -0300

    feat: use .env vars

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('Database URL not informed')
+}
+
 export const databaseConfig: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
