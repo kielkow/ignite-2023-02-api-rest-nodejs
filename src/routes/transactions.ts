7e96e70976575762eade2527e6d427f33f02027a
kielkow <matheuskiel@fiorifer.com.br>
2023-05-30 13:08:09 -0300

    feat: implment zod to valid transaction body types

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 
@@ -6,6 +7,7 @@ import { knex } from '../database'
 interface ITransactionBody {
   title: string
   amount: number
+  type: 'credit' | 'debit'
 }
 
 interface ITransactionQueryString {
@@ -36,13 +38,26 @@ export async function transactionRoutes(app: FastifyInstance) {
   })
 
   app.post<{ Body: ITransactionBody }>('/', {}, async (req, res) => {
-    const { title, amount } = req.body
+    const transactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    let transactionBody: ITransactionBody
+    try {
+      transactionBody = transactionBodySchema.parse(req.body)
+    } catch (error) {
+      return res.status(400).send(JSON.parse(String(error)))
+    }
+
+    const { title, amount, type } = transactionBody
 
     const transaction = await knex('transactions')
       .insert({
         id: randomUUID(),
         title,
-        amount,
+        amount: type === 'credit' ? amount : amount * -1,
       })
       .returning('*')
 
