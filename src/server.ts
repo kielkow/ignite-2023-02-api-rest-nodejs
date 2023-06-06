0675fac38dcb8d11f7a4bd3cbb87b9ddb7ea1c29
kielkow <matheuskiel@fiorifer.com.br>
2023-06-06 10:47:46 -0300

    feat: implement middleware log request

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
-import { knex } from './database'
 import { env } from './env'
+import { knex } from './database'
+import { logRequest } from './middleware/log-request'
 import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
 app.register(cookie)
+
+app.addHook('preHandler', logRequest)
 
 app.get('/status', () => {
   return 'server is running'
