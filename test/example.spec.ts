135a5cdf483abaca02088f9224d0cd758c113c23
kielkow <matheuskiel@fiorifer.com.br>
2023-06-07 11:42:22 -0300

    test: user can list transactions

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -20,3 +20,24 @@ test('user can create a new transaction', async () => {
 
   expect(response.statusCode).toEqual(201)
 })
+
+test('user can list transactions', async () => {
+  const responseCreateTransaction = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'new_transaction',
+      amount: 1000,
+      type: 'credit',
+    })
+
+  const sessionId = responseCreateTransaction.headers['set-cookie'][0]
+
+  const response = await request(app.server)
+    .get('/transactions')
+    .set('Cookie', sessionId)
+
+  expect(response.statusCode).toEqual(200)
+  expect(response.body).toBeTruthy()
+  expect(response.body.total).toEqual(1)
+  expect(response.body.transactions.length).toEqual(1)
+})
